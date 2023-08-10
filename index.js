dd9a83f9354e1c51c7deb1b58712ef9ed00ae352
Deepanshu Jain <deepanshujain088@gmail.com>
2023-08-10 23:26:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node has been running gooddd. I mean extra good! And now its on auto build\n'
+  const msg = 'Hello Node has been running good\n'
   res.end(msg);
 });
 
