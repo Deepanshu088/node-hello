1ab1e3b258b9ef9734488fda32c68b2b48914bc4
Deepanshu Jain <deepanshujain088@gmail.com>
2023-08-10 22:12:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node j jyayhadfhoashjeo!\n'
+  const msg = 'Hello Node has been running gooddd!\n'
   res.end(msg);
 });
 
