59949be359398afb9c74722a788a95562574a37d
Deepanshu Jain <deepanshujain088@gmail.com>
2023-08-10 23:32:23 +0530

    last commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node has been running good\n'
+  const msg = 'Hello Node has been running well. And its automatic build here. :) \n'
   res.end(msg);
 });
 
